6a3e1fbc4b70fef131c0e1c526fec742ba069f90
MatteoTP06 <matteo.torquatoperillo@elev.ga.ntig.se>
2025-02-18 10:10:55 +0100

    18/02

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,1 +1,9 @@
-setInterval(() => console.log(new Date().toTimeString()),1000);
+var localClock = document.getElementById('localClock');
+var skyClock = document.getElementById('skyClock');
+
+window.onload = UpdateClock();
+function UpdateClock(){
+    localClock.textContent = (new Date().toLocaleTimeString())
+    skyClock.textContent = (new Date().toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles', hour12: false }))
+    setTimeout(UpdateClock, 1000)
+}
